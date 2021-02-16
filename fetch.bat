25b7f6e8f08b22d26d81d6bad2287c6dc2dfa4de
Sillanpää Viljami Jannenpoika <viljami.sillanpaa@edu.tampere.fi>
2021-02-16 10:03:14 +0200

    Updated fetch.bat

--- fetch.bat
+++ fetch.bat
@@ -1,5 +1,10 @@
 @echo off
 
-echo "Copying..."
+
+IF exist "teht" (
+	echo Removing old files
+	rmdir /S /Q teht
+)
+echo Copying...
 Xcopy /E /I O:\"Palvelut ja liiketalous"\"Eerikki Maula"\2008TiviPOk02_sasp .\teht\
-echo "Done!"
+echo Done!
