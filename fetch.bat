73e0e8525db7933dba4b0ce73c1808372497b189
Vildis <viljami.sillanpaa@edu.tampere.fi>
2021-02-23 13:37:03 +0200

    Backup ti 23.02.2021

--- fetch.bat
+++ fetch.bat
@@ -1,13 +1,13 @@
 @echo off
-echo [92mCopying teht...[0m
+echo [92mCopying teht...[33m
 robocopy O:\\"Palvelut ja liiketalous"\\"Eerikki Maula"\2008TiviPOk02_sasp .\teht\
 
-echo [92mCopying eerikki...[0m
+echo [92mCopying eerikki...[33m
 
 robocopy S:\eerikki\php .\php\eerikki\
-echo [92mBacking up for remote[0m
+echo [92mBacking up for remote[33m
 IF exist *-backup.zip (
-	echo [92mRemoving old backup[0m
+	echo [92mRemoving old backup[33m
 	del *-backup.zip
 )
 tar -c -f "%date%"-backup.zip *
@@ -17,18 +17,18 @@ git add *
 git commit -m "Backup %date%"
 setlocal
 :PROMPT
-SET /P AREYOUSURE=[41mAre you sure[0m ([41mY[0m/[92m[N][0m)?
+SET /P AREYOUSURE=[31mAre you sure[33m ([31mY[33m/[92m[N][33m)?
 IF /I "%AREYOUSURE%" NEQ "y" GOTO END
 PAUSE
 
-echo [93m100% sure???[0m
+echo [93m100% sure???[33m
 PAUSE
 
-echo [93malright last one, you sure???[0m
+echo [93malright last one, you sure???[33m
 PAUSE
 
 git push
-echo [92mDone![0m
+echo [92mDone![33m
 
 :END
 endlocal
