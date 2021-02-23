00230616e04f6468c52ee8418fa7558aa9df9a00
Vildis <viljami.sillanpaa@edu.tampere.fi>
2021-02-23 09:25:09 +0200

    Erotettu eerikki ja mun

--- fetch.bat
+++ fetch.bat
@@ -1,10 +1,17 @@
 @echo off
 
 
-IF exist "teht" (
+IF exist ".\teht" (
 	echo Removing old files
 	rmdir /S /Q teht
 )
 echo Copying...
 Xcopy /E /I O:\"Palvelut ja liiketalous"\"Eerikki Maula"\2008TiviPOk02_sasp .\teht\
+
+echo Copying eerikki...
+IF exist .\php\eerikki (
+	echo Removing old files
+	rmdir /S /Q php\eerikki
+)
+Xcopy /E /I S:\eerikki\php .\php\eerikki\
 echo Done!
