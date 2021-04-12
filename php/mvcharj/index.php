140c01ef17226b38a655cb7d0f2d9d1ac293d7c3
Vildis <viljami.sillanpaa@edu.tampere.fi>
2021-04-12 09:00:25 +0300

    changed folder structure

--- php/mvcharj/index.php
+++ php/mvcharj/index.php
@@ -1,3 +1,42 @@
 <?php
+session_start();
+
+class Model {
+    private $text;
+    public function __construct($text = 'Hello World') {
+        $this->text = $text;
+    }
+    public function getText() {
+        return $this->text;
+    }
+    public function setText($text) {
+        return new Model($text);
+    }
+}
+class View {
+    public function output(Model $model) {
+        return '<a href="index.php?action=textclicked">' . $model->getText() . '</a>';
+    }
+}
+class Controller {
+    public function notLogged(Model $model): model {
+        return $model->setText('Index page');
+    }
+
+    public function textClicked(Model $model): Model {
+        return $model->setText('Text Clicked');
+    }
+}
+$model = new Model();
+$controller = new Controller();
+$view = new View();
+
+if (isset($_GET['action'])) {
+    $model = $controller->{$_GET['action']}($model);
+} else {
+    $model = $controller->notLogged($model);
+}
+echo $view->output($model);
+
 
 ?>
--- php/mvcharj/models/news.php
+++ php/mvcharj/models/news.php
@@ -8,4 +8,22 @@ Luo siihen seuraavat funktiot ja testaa niiden toiminta (voit myös vaihtaa nime
     muokkaaUutista($data)
     poistaUutinen($id)
 
+<?php
+
+require("./database/connection.php");
+
+function get_all_news()
+{
+    global $pdo;
+
+    $sql = "SELECT * FROM news";
+    $stm = $pdo->query($sql);
+
+    $news = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+    return $news;
+
+}
+
+?>
 
