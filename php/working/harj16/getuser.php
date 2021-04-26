9f69d60bbe63d5d4adb1827e87a5b1d556c7a567
Vildis <viljami.sillanpaa@edu.tampere.fi>
2021-04-26 10:34:38 +0300

    siirrelty asioita

--- php/working/harj16/getuser.php
+++ php/working/harj16/getuser.php
@@ -19,7 +19,33 @@ th {text-align: left;}
 <body>
 
 <?php
-$q = intval($_GET['q']);
+
+function sanit($input)
+{
+    $input = trim($input); //poistaa tyhjät välilyönnit alusta ja lopusta
+    $input = filter_var($input, FILTER_SANITIZE_STRING);
+    return $input;
+}
+
+function makeArticle($article)
+{
+    $articleId = (int)$article["articleId"];
+    
+    $title = sanit($article["title"]);
+    $content = sanit($article["content"]);
+
+    $beautified = "<div style='border: 1px solid black;margin-bottom: 20px; margin-top: 20px;padding-left: 10px' id='$articleId'><h2>$title</h2><div><p>$content</p>";
+
+    $beautified .= "</div></div>";
+    return $beautified;
+}
+
+?>
+
+<?php
+
+
+$q = $_GET['q'];
 
 $con = mysqli_connect('samarium','20silvil','salasana','20silvil');
 if (!$con) {
@@ -27,7 +53,8 @@ if (!$con) {
 }
 
 mysqli_select_db($con, "20silvil");
-$sql="SELECT * FROM h11_characters WHERE charID = '".$q."'";
+
+$sql="SELECT * FROM mvc2_articles WHERE title LIKE '%$q%' OR content LIKE '%$q%'";
 $result = mysqli_query($con, $sql);
 
 echo "<table>
@@ -35,14 +62,7 @@ echo "<table>
 </tr>";
 while($row = mysqli_fetch_array($result)) {
   echo "<tr>";
-  echo "<td>" . $row['name'] . "</td>";
-  echo "<td>" . $row['level'] . "</td>";
-  echo "<td>" . $row['strength'] . "</td>";
-  echo "<td>" . $row['dexterity'] . "</td>";
-  echo "<td>" . $row['wisdom'] . "</td>";
-  echo "<td>" . $row['charisma'] . "</td>";
-  echo "<td>" . $row['intelligence'] . "</td>";
-  echo "<td>" . $row['hitpoints'] . "</td>";
+  echo makeArticle($row);
   echo "</tr>";
 }
 echo "</table>";
