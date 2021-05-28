4331cf49bbe7fab6426560247a6f9a618824e24b
Vildis <viljami.sillanpaa@edu.tampere.fi>
2021-05-28 14:56:54 +0300

    uutta indexiin

--- php/partials/navi.php
+++ php/partials/navi.php
@@ -1,24 +1,34 @@
 <?php
+echo "<h2>Luokkaharj</h2>";
+echo "<a href='./luokkaharj/'>MVC Harjoitus</a>";
+echo "<h2>XML-Harjoitus 1</h2>";
+echo "<a href='../xml/xmlharj1.php'>MVC Harjoitus</a>";
+echo "<h2>MVC Harjoitus</h2>";
+echo "<a href='../mvcharj/'>MVC Harjoitus</a>";
 echo "<h2>MVC Harjoitus</h2>";
 echo "<a href='../mvcharj/'>MVC Harjoitus</a>";
 
 
-$paths = array("harj", "demo", "partials");
+$paths = array("harj", "demo", "partials", "isot");
 foreach($paths as $path) {
 
-    if($path === "harj")
-        echo "<h2>Harjoitukset</h2>";
-    elseif($path === "demo")
-        echo "<h2>Demot</h2>";
-    else
-        echo "<h2>Partials</h2>";
+    switch($path){
+        case "harj":
+            echo "<h2>Harjoitukset</h2>";
+        break;
+        case "demo":
+            echo "<h2>Demot</h2>";
+        break;
+        case "partials":
+            echo "<h2>Partials</h2>";
+        break;
+    }
 
     if ($handle = opendir("./".$path)) {
         while (false !== ($file = readdir($handle))) {
             if ('.' === $file) continue;
             if ('..' === $file) continue;
-
-        
+            
             echo "<a href='./?sivu=".preg_replace("(\.php)", "", $file)."&kansio=$path'>".ucfirst(preg_replace("(\.php)", "", $file))."</a><br>";
         }
         closedir($handle);
