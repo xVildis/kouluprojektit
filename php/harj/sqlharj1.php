c97f1d6b498f3645890a7c40c0ae5c8826c098d7
Sillanpää Viljami Jannenpoika <viljami.sillanpaa@edu.tampere.fi>
2021-06-02 11:09:32 +0300

    sql harj 1 helppoa

--- php/harj/sqlharj1.php
+++ php/harj/sqlharj1.php
@@ -10,11 +10,14 @@
 
 <li>Hae kaikki keikat päivämäärän mukaan nousevaan järjestykseen järjestettynä (ASC). Nyt lisätään kyselyn loppuun ORDER BY-termi.
 
-<pre></pre>
+<pre>SELECT * FROM keikat ORDER BY ASC</pre>
 
 </li>
 <li>Hae kaikki esiintyjät joiden sukunimi alkaa tietyllä kirjaimella. Nyt tarvitset ehdossa LIKE-termiä ja katkaisumerkkiä.</li>
+<pre>SELECT * FROM esiintyjät WHERE sukunimi LIKE %a%</pre>
+
 <li>Tee kysely, jonka avulla saat selville kaikkien managerien tiedot nimien mukaisessa aakkosjärjestyksessä (sukunimi ensisijainen ja etunimi toissijainen lajittelukenttä).</li>
-<li>jne.</li>
+<pre>SELECT * FROM managerit ORDER BY sukunimi, etunimi ASC</pre>
+
 </ol>
 </html>
